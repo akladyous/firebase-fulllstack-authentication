d79480f5991c01bc5348110625e84f0c9a074e61
Boula Akladyous <akladyous@gmail.com>
2022-07-13 17:51:38 -0400

    delete test files

--- backend/routes/shortUrl/shortUrl.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-export const shortUrlRoute = express.Router();
-import { get } from '../../controllers/get.js'
-import { post } from '../../controllers/post.js'
-
-shortUrlRoute.get('/shorturl', get);
-shortUrlRoute.post('/shorturl', post);
-
